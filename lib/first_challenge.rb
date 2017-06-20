46140aa14d5d072b02705f62a5b6963c3d72d507
yuralala8 <yc1435@nyu.edu>
2017-06-19 21:47:23 -0400

    manipulating hashes commit

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -12,10 +12,19 @@ def first_challenge
       favorite_icecream_flavors: ["strawberry", "cookie dough", "mint chip"]
     }
   }
-
-  #your code here
-
-
+  
+contacts.each do |person, data|
+    if person == "Freddy Mercury"
+        data.each do |attribute, value|
+            if attribute == :favorite_icecream_flavors
+                value.delete_if do |flavor|
+                    flavor = "strawberry"
+                    end
+                end
+            end
+        end
+    end
+                
   #remember to return your newly altered contacts hash!
   contacts
 end
